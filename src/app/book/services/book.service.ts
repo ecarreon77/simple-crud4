79f7fe9db6c57b44333f397b0ae98976677911ab
ferminpatdujr <fpatdu@77global.biz>
2022-08-21 23:53:29 +0800

    push

--- src/app/book/services/book.service.ts
+++ src/app/book/services/book.service.ts
@@ -5,88 +5,63 @@ import { Observable, tap } from 'rxjs';
 import { Book } from '../models/book';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BookService {
-    id:number = 4
-    editedBook:Book[]|undefined
-    addFlag = false;
-    editFlag = false;
-    books : Book[] = [
-      {id : 1, name: "	The Grass is Always Greener", authors:["Jeffrey Archer"], isbn: "1-86092-049-7" },
-      {id : 2, name: "	Murder!", authors:["Arnold Bennett (1867-1931)"], isbn: "1-86092-012-8" },
-      {id : 3, name: "A Boy at Seven", authors:["John Bidwell"], isbn: "1-86092-022-5" }
+  id: number = 4;
+  editedBook: Book[] | undefined;
+  addFlag = false;
+  editFlag = false;
+  books: Book[] = [
+    {
+      id: 1,
+      name: '	The Grass is Always Greener',
+      authors: ['Jeffrey Archer'],
+      isbn: '1-86092-049-7',
+    },
+    {
+      id: 2,
+      name: '	Murder!',
+      authors: ['Arnold Bennett (1867-1931)'],
+      isbn: '1-86092-012-8',
+    },
+    {
+      id: 3,
+      name: 'A Boy at Seven',
+      authors: ['John Bidwell'],
+      isbn: '1-86092-022-5',
+    },
+  ];
 
-    ];
+  editedForm: FormGroup | undefined;
+  constructor(private fb: FormBuilder, private http: HttpClient) {}
 
-    editedForm : FormGroup | undefined
-    constructor(private fb : FormBuilder, private http : HttpClient) { }
-
-    getBooks(): Observable<Book[]>{
-      return this.http.get<Book[]>("http://localhost:3000/books")
-    }
-
-    addBooks(book : Book){
-      return this.http.post("http://localhost:3000/books", book)
-    }
-
-    editBook(book : Book){
-      return this.http.put(`http://localhost:3000/books/${book.id}`, book)
-    }
-
-    delete(book : Book){
-      return this.http.delete(`http://localhost:3000/books/${book.id}`)
-    }
-//-----------------------------------------------------------------------------------------------------
-
-    // getBooks(){
-    //   return this.books
-
-    // }
-
-    // addBooks(book:Book){
-    //   book.id = this.id
-    //   this.id++
-    //   this.books.push(book)
-
-    // }
-
-    editBookForm(id:number, books : Book[]){
-      this.editedBook = books.filter((book:Book) =>{
-        if(book.id === id){
-          return book
-        }
-      })
-      return this.editedBook
+  getBooks(): Observable<Book[]> {
+    return this.http.get<Book[]>('http://localhost:3000/books');
   }
 
-  // editBook(newBook : Book){
-  //    this.books.map(book =>{
-  //     if(book.id == newBook.id){
-  //       book.name = newBook.name;
-  //       book.authors = newBook.authors;
-  //       book.isbn = newBook.isbn;
-  //       console.log("from services" + book.id)
-  //       console.log("newBook Id " + newBook.id)
-  //       console.log(this.books)
-  //     }
-  //   })
-  //   console.log("newBook " + newBook.id)
-
-  //}
-  deleteAll(){
-    return this.books = []
+  addBooks(book: Book) {
+    return this.http.post('http://localhost:3000/books', book);
   }
 
-  // delete(id: number){
-  //   console.log("service "+id)
-  //   this.books = this.books.filter(book =>{
-  //     if(book.id !== id){
-  //       return book
-  //     }
-  //   })
-  //   return this.books
-  // }
+  editBook(book: Book) {
+    return this.http.put(`http://localhost:3000/books/${book.id}`, book);
+  }
 
+  delete(book: Book) {
+    return this.http.delete(`http://localhost:3000/books/${book.id}`);
+  }
+
+  editBookForm(id: number, books: Book[]) {
+    this.editedBook = books.filter((book: Book) => {
+      if (book.id === id) {
+        return book;
+      }
+    });
+    return this.editedBook;
+  }
+
+  deleteAll() {
+    return (this.books = []);
+  }
 }
-
